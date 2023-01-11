661c90e88bb07db50e65c7546ed36ef2ffed71cb
prism <veronique.vdang@gmail.com>
2023-01-11 15:55:26 +0100

    setting up standard version for dev branch

---
+++ src/pages/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div className='main_container'>
+        <video src=""></video>
+        <div className='log_box'>
+            <p>Google (and other) Authentification</p>
+        </div>
+    </div>
+  )
+}
+
+export default Login
