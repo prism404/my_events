93d51aa87649c160540689e7258dbf38c031cc7b
gaelWumba <gael.wumba-buatu@epitech.eu>
2023-01-24 12:34:01 +0100

    Search final..

--- my_event-client/src/components/FetchEvents.js
+++ my_event-client/src/components/FetchEvents.js
@@ -18,7 +18,7 @@ function FetchEvents() {
   const [displayPerPage, setDisplayPerPage ] = useState(10);
   const [currentPage, setCurrentPage] = useState(1);
 
-  const [search, setSearch] = useState();
+  const [search, setSearch] = useState("");
 
   const numOfPages = Math.ceil(events.length / displayPerPage);
   const pages = [...Array(numOfPages + 1).keys()].slice(1);
@@ -29,21 +29,27 @@ function FetchEvents() {
 
 
   useEffect(() => {
-    if(search){
-      setEvents([search])
-    }else{
     axios({
       method: "GET",
       url: "https://public.opendatasoft.com//api/records/1.0/search/?dataset=evenements-publics-openagenda&q=&rows=100",
-      headers: { "content-type": "application/json" },
+      headers: {
+        "content-type": "application/json"
+      },
     }).then((res) => {
         setEvents(res.data.records);
+        console.log(res.data.records);
       })
       .catch((error) => {
-        console.log(error);
+        if(error.res){
+          console.log(error.res);
+        }else if(error.request){
+          console.log(error.request);
+        }else if(error.message){
+          console.log(error.message);
+        }
       });
-    return;}
-  }, [search]);
+    return;
+  }, []);
 
 
   const prevHandler = () => {
@@ -54,76 +60,30 @@ function FetchEvents() {
     if (currentPage !== numOfPages ) setCurrentPage(currentPage + 1);
   }
   
-  const handleSearch = (e, v) => {
-    if (e.key === "Enter"){
-      setSearch(v);
-    }
-  }
-  
   if (!events) return <div>Nothing to see here!</div>;
   return (
     <div className='fetch_container'>
-      <Box className='search'>
-        <Autocomplete
-          freeSolo
-          options={events}
-          sx={{ width: 300 }}
-          getOptionLabel={(results) => results.fields.title_fr}
-          noOptionsText={"Sorry, but I couldn't find anything..."}
-          onChange={(e, v) => setSearch(v)}
-      
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              variant="standard"
-              label="Evenement"
-              placeholder='Evenement'
-              className="rounded-full"
-            />
-          )}
+
+      <div className='search'>
+        <input 
+          type="text" 
+          placeholder='Search...'
+          onChange={(e) => setSearch(e.target.value)}
         />
-        <Autocomplete
-          freeSolo
-          options={events}
-          sx={{ width: 300 }}
-          getOptionLabel={(results) => results.fields.keywords_fr}
-          noOptionsText={"Sorry, but I couldn't find anything..."}
-          onChange={(e, v) => setSearch(v)}
-      
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              variant="standard"
-              label="Type d'evenement"
-              placeholder='Type'
-              className="rounded-full"
-            />
-          )}
-        />
-        <Autocomplete
-          freeSolo
-          options={events}
-          sx={{ width: 300 }}
-          getOptionLabel={(results) => results.fields.location_region}
-          noOptionsText={"Sorry, but I couldn't find anything..."}
-          onChange={(e, v) => setSearch(v)}
-          onKeyDown={handleSearch}
-      
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              variant="standard"
-              label="Region"
-              placeholder="Lieu de l'evenement"
-              className="rounded-full"
-            />
-          )}
-        />
-      </Box>
+      </div>
 
       <h2>Evenements disponible</h2>
       <div className='events-grid'>
-        {visibleEvent.map((data) => (
+        {visibleEvent.filter((value) => {
+            if(search === ""){
+              return value;
+            }else if(
+              value.fields.title_fr.toLowerCase().includes(search.toLowerCase()) || 
+              value.fields.location_city.toLowerCase().includes(search.toLowerCase()) ||
+              value.fields.location_address.toLowerCase().includes(search.toLowerCase())){
+              return value;
+            }
+          }).map((data) => (
           <div key={data.recordid} className='card'>
             <div className='card_contents'>
               <Link to={`/event/${data.fields.uid}`}>
@@ -134,6 +94,7 @@ function FetchEvents() {
                 <h4>{data.fields.daterange_fr}</h4>
                 <div className='event_infos'>
                   <div>
+                    <h4>Adresse: {data.fields.location_address}</h4>
                     <h4>Ville: {data.fields.location_city}</h4>
                     <h4>Region: {data.fields.location_region}</h4>
                     <h4>Pays : {data.fields.country_fr}</h4>
